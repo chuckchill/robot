b9490ff6bba1372b22042f83353e9f035bfceaa5
albert <260381980@qq.com>
2018-06-06 21:59:42 +0800

    12

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 
 $api->get('login', 'IndexController@login');
+$api->get('startup-page', ['uses' => 'AppConfigController@startupPpage']);//启动页
+$api->get('link-page', ['uses' => 'AppConfigController@linkPage']);//引导页
 $api->group(['middleware' => ['api.auth']], function ($api) {
     $api->get('test', ['uses' => 'IndexController@index']);
 });
