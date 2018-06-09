00fb657c0d647bc70ae57446cbe7dcc9631503a2
albert <260381980@qq.com>
2018-06-09 09:40:06 +0800

    index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>深圳华泰思安信息技术有限公司</title>
+    <link rel="stylesheet" type="text/css" href="/robot/css/index.css"/>
+</head>
+<body>
+<div class="big-box" id="bigBox">
+    <div class="item item1">
+        <img src="/robot/images/banner.jpg">
+    </div>
+    <div class="item item2">
+        <div class="remarks img-left">
+            <div class="text">
+                <p>
+                    教育型的机器人。主要是针对不同的人群进行不同的教学，以最大程度的开发学生的潜质，目前的教育机器人的教学已经在小学，中学和大学的各个层次展开，它能够提供多种的编程平台，让大家自己进行自由和组合和拆卸，并且还可以让学生自行设计出某些的零部件。小学生在使用教育机器人的时候，由于编译方面的水平限制，只能使用已经编译好的程序命令，进行相应模拟指令，这对于发挥孩子的创造力也是很有帮助的。
+                </p>
+            </div>
+            <div>
+                <img src="/robot/images/robot-headphones.jpg">
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="remarks img-right">
+            <div class="text">
+                <p>
+                    工业机器人是面向工业领域的多关节机械手或多自由度的机器装置，它能自动执行工作，是靠自身动力和控制能力来实现各种功能的一种机器。它可以接受人类指挥，也可以按照预先编排的程序运行，现代的工业机器人还可以根据人工智能技术制定的原则纲领行动。
+                </p>
+            </div>
+            <div>
+                <img src="/robot/images/shutterstock.jpg">
             </div>
         </div>
-    </body>
+        <div class="remarks img-left">
+            <div class="text">
+                <p>
+                    比赛型的教育机器人。主要是用于各类机器人大赛中。小学阶段也是有比赛型的机器人的，它通常是提供一些标志的程序和零件，让大家进行适当的改动，这个主要是针对机器人水平很高的孩子们使用的，这样让孩子们从小就能够感受到机器人教育的重要。
+                </p>
+            </div>
+            <div>
+                <img src="/robot/images/th.jpg">
+            </div>
+        </div>
+    </div>
+    <div class="item item3">
+        <img src="">
+    </div>
+</div>
+
+<script src="/robot/js/index.js"></script>
+</body>
 </html>
