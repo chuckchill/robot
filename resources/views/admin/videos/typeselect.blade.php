d8dc90372cc7c8a580e6d3e67a7ff120c2f53546
albert.qiu <shaochang.qiu@bqjr.cn>
2018-06-29 15:20:48 +0800

    selelct

--- resources/views/admin/videos/typeselect.blade.php
+++ resources/views/admin/videos/typeselect.blade.php
@@ -1,7 +1,7 @@
 <select id="videoType" class="form-control" name="type">
     @foreach(\App\Services\ModelService\VideosType::getTypeTree() as $trees)
         <optgroup label="{{$trees['name']}}">
-            @foreach(array_get($trees,"children") as $key=>$item)
+            @foreach(array_get($trees,"children",[]) as $key=>$item)
                 <option @if($key==$select) selected @endif value="{{$key}}">
                         {{$item['name']}}
                 </option>
