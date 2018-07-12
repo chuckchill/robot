318ccca04ed3bbf7be0a1c093d6d8b6bfc7c1dda
albert.qiu <shaochang.qiu@bqjr.cn>
2018-07-12 16:26:21 +0800

    wed

--- resources/views/admin/videos/index.blade.php
+++ resources/views/admin/videos/index.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     <div class="row page-title-row" id="dangqian" style="margin:5px;">
         <div class="col-md-12 text-right">
-            @if(Gate::forUser(auth('admin')->user())->check('admin.videos.create'))
+            @if(request()->get("upload"))
                 <a href="/admin/videos/create" class="btn btn-success btn-md"><i
                             class="fa fa-plus-circle"></i> 添加视频 </a>
             @endif
