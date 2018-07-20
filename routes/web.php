2cdb18d4abf3ee3ed17e1e59909460828703904c
albert <260381980@qq.com>
2018-07-20 22:00:51 +0800

    doc

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/app', 'HomeController@getApp');
+Route::get('/apidoc', 'HomeController@apiDoc');
 Route::get('/qiniu/index', 'QiniuController@index');
 Route::any('/qiniu/backend-video-callback', ['as' => 'qiniu.backend_video_callback', 'uses' => 'QiniuController@backendVideoCallback']);
 Route::any('/qiniu/user-upload-callback', ['as' => 'qiniu.user-upload-callback', 'uses' => 'QiniuController@userUploadCallback']);//七牛上传回调
